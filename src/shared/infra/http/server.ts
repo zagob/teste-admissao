f2f730476d0a583186a9ea21c9210fa2caab021e
zagob <matheusus1_23@hotmail.com>
2022-03-03 14:54:06 -0300

    add cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,11 +3,13 @@ import "reflect-metadata";
 import "express-async-errors";
 import { AppError } from "../../errors/AppError";
 import "../container";
+import cors from "cors";
 import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.use(
